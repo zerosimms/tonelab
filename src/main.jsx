3c80a16c6338d947589b775aa5d1ce895d53c51a
unknown <bensimms2002@yahoo.co.uk>
2026-07-03 21:41:34 +0100

    inital push

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ToneLab from "./ToneLab.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ToneLab />
+  </React.StrictMode>
+);
